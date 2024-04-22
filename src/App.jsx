a5917fbf60113c8d07ad65a56e08368e29273ae5
sirdashasan <sirdashasan@hotmail.com>
2024-04-22 14:06:23 +0300

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+
 function App() {
   return (
-    <>
-      <h1>App</h1>
-    </>
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/shop">
+          <Shop />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/blog">
+          <Blog />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
