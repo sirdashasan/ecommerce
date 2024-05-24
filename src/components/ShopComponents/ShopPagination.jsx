18db186f788e336cc5b6952fc25dac0aa40c0d5f
sirdashasan <sirdashasan@hotmail.com>
2024-05-24 11:43:47 +0300

    T15 tamamlandı

--- src/components/ShopComponents/ShopPagination.jsx
+++ src/components/ShopComponents/ShopPagination.jsx
@@ -1,44 +1,59 @@
-import React, { useState } from "react";
+import React from "react";
+import ReactPaginate from "react-paginate";
+import { useDispatch, useSelector } from "react-redux";
+import { setOffset } from "../../store/actions/productActions";
+import { getProducts } from "../../store/thunks/productThunks";
+import { useHistory, useLocation } from "react-router-dom";
 
 const ShopPagination = () => {
-  const [activePage, setActivePage] = useState(2);
-  const totalPages = 3;
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const location = useLocation();
+  const total = useSelector((state) => state.product.total);
+  const limit = useSelector((state) => state.product.limit);
+  const offset = useSelector((state) => state.product.offset);
+  const pageCount = Math.ceil(total / limit);
 
-  const handlePageChange = (pageNumber) => {
-    if (pageNumber >= 1 && pageNumber <= totalPages) {
-      setActivePage(pageNumber);
-    }
+  const handlePageClick = (data) => {
+    const selectedPage = data.selected;
+    const newOffset = selectedPage * limit;
+
+    const params = new URLSearchParams(location.search);
+    params.set("limit", limit);
+    params.set("offset", newOffset);
+    history.push({ search: params.toString() });
+
+    dispatch(setOffset(newOffset));
+    dispatch(getProducts(null, null, null, limit, newOffset));
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   return (
     <div className="flex justify-center items-center mb-12 font-sans font-bold">
       <div className="border-2 rounded-lg overflow-hidden flex bg-white">
-        <button
-          onClick={() => handlePageChange(1)}
-          className={`px-4 py-2 text-sm border-r `}
-        >
-          First
-        </button>
-        {[1, 2, 3].map((number) => (
-          <button
-            key={number}
-            onClick={() => handlePageChange(number)}
-            className={`px-4 py-2 text-sm border-r ${
-              number === activePage
-                ? "bg-[#23A6F0] text-white"
-                : "hover:bg-gray-100 text-[#23A6F0]"
-            }`}
-          >
-            {number}
-          </button>
-        ))}
-        <button
-          onClick={() => handlePageChange(activePage + 1)}
-          className={`px-4 py-2 text-sm `}
-          disabled={activePage === totalPages}
-        >
-          Next
-        </button>
+        <ReactPaginate
+          previousLabel={"First"}
+          nextLabel={"Next"}
+          breakLabel={"..."}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={3}
+          onPageChange={handlePageClick}
+          containerClassName={"pagination flex"}
+          pageClassName={
+            "px-4 py-2 text-sm border-r hover:bg-gray-100 text-[#23A6F0] flex items-center justify-center"
+          }
+          activeClassName={"bg-[#23A6F0] text-white"}
+          previousClassName={
+            "px-4 py-2 text-sm border-r hover:bg-gray-100 text-[#23A6F0] flex items-center justify-center"
+          }
+          nextClassName={
+            "px-4 py-2 text-sm hover:bg-gray-100 text-[#23A6F0] flex items-center justify-center"
+          }
+          breakClassName={
+            "px-4 py-2 text-sm border-r hover:bg-gray-100 text-[#23A6F0] flex items-center justify-center"
+          }
+        />
       </div>
     </div>
   );
