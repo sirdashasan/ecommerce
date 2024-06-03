9f6f1d263fa6bf2ed3bd329e60a325083115a914
sirdashasan <sirdashasan@hotmail.com>
2024-06-03 17:55:54 +0300

    T17 görevi tamamlandı

--- src/store/reducers/shoppingCartReducer.js
+++ src/store/reducers/shoppingCartReducer.js
@@ -1,14 +1,16 @@
+import {
+  SET_CART,
+  SET_PAYMENT,
+  SET_ADDRESS,
+  ADD_TO_CART,
+  SET_SHOW_CART,
+} from "../actions/shoppingCartActions";
+
 const initialState = {
   cart: [],
   payment: {},
   address: {},
 };
-
-import {
-  SET_CART,
-  SET_PAYMENT,
-  SET_ADDRESS,
-} from "../actions/shoppingCartActions";
 
 const shoppingCartReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -26,6 +28,31 @@ const shoppingCartReducer = (state = initialState, action) => {
       return {
         ...state,
         address: action.payload,
+      };
+    case ADD_TO_CART:
+      const existingProductIndex = state.cart.findIndex(
+        (item) => item.product.id === action.payload.id
+      );
+      if (existingProductIndex !== -1) {
+        const newCart = [...state.cart];
+        newCart[existingProductIndex].count += 1;
+        return {
+          ...state,
+          cart: newCart,
+        };
+      } else {
+        return {
+          ...state,
+          cart: [
+            ...state.cart,
+            { count: 1, checked: true, product: action.payload },
+          ],
+        };
+      }
+    case SET_SHOW_CART:
+      return {
+        ...state,
+        showCart: action.payload,
       };
     default:
       return state;
