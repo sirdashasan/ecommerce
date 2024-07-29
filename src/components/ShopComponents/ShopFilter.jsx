3f8fe9950b6d555af8c551a1a9983fd1dcea4c46
sirdashasan <sirdashasan@hotmail.com>
2024-07-29 16:03:02 +0300

    filter bölümü responsive düzenlendi

--- src/components/ShopComponents/ShopFilter.jsx
+++ src/components/ShopComponents/ShopFilter.jsx
@@ -26,12 +26,11 @@ const ShopFilter = () => {
 
   return (
     <div className="flex flex-col items-center justify-between rounded font-sans mt-12 md:mx-4">
-      <div className="flex flex-col md:flex md:flex-row md:items-center md:justify-between md:w-full ">
-        <div className="text-sm font-semibold text-center text-[#737373]">
-          Showing all 12 results
+      <div className="flex flex-col items-center md:flex-row md:items-center md:justify-between md:w-full">
+        <div className="text-sm font-semibold text-center text-[#737373] mb-4 md:mb-0">
+          Filter or sort products
         </div>
-
-        <div className="text-[#737373] mr-4 mt-4 md:mt-0">
+        <div className="flex items-center justify-center text-[#737373] mb-4 md:mb-0 md:mr-4">
           <span className="mr-4 font-semibold">Views:</span>
           <button className="border-2 border-[#ECECEC] rounded-md w-10 h-10 mr-2">
             <FontAwesomeIcon
@@ -43,18 +42,18 @@ const ShopFilter = () => {
             <FontAwesomeIcon icon={faList} className="cursor-pointer mx-1" />
           </button>
         </div>
-        <div className="mr-4 mt-4 md:mt-0 flex items-center">
+        <div className="flex flex-col items-center w-full md:flex-row md:w-auto">
           <input
             type="text"
             value={filter}
             onChange={handleFilterChange}
             placeholder="Filter"
-            className="border border-gray-300 bg-[#F9F9F9] rounded py-2 px-3 text-gray-700 mr-2"
+            className="border border-gray-300 bg-[#F9F9F9] rounded py-2 px-3 text-gray-700 mb-4 md:mb-0 md:mr-2 w-full md:w-auto"
           />
           <select
             value={sort}
             onChange={handleSortChange}
-            className="border border-gray-300 bg-[#F9F9F9] rounded py-2 px-3 text-gray-700 mr-2"
+            className="border border-gray-300 bg-[#F9F9F9] rounded py-2 px-3 text-gray-700 mb-4 md:mb-0 md:mr-2 w-full md:w-auto"
           >
             <option value="popularity">Sort</option>
             <option value="price:asc">Price: Low to High</option>
@@ -64,7 +63,7 @@ const ShopFilter = () => {
           </select>
           <button
             onClick={handleFilterClick}
-            className="bg-[#23A6F0] text-white font-bold py-2 px-4 rounded"
+            className="bg-[#23A6F0] text-white font-bold py-2 px-4 rounded w-full md:w-auto"
           >
             Filter
           </button>
