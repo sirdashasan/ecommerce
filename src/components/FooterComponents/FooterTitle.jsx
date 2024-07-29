a8eaa7fdbb8e8d697107c5608d74a1e679ab4a13
sirdashasan <sirdashasan@hotmail.com>
2024-07-29 15:38:27 +0300

    practice advice fashion tips bölümü güncellendi

--- src/components/FooterComponents/FooterTitle.jsx
+++ src/components/FooterComponents/FooterTitle.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const FooterTitle = () => {
+  const history = useHistory();
+
+  const navigateToLogo = () => history.push("/");
+
   return (
     <div>
       <h1 className="text-2xl font-bold text-[#252B42] md:text-3xl">
-        <a href="#">Bandage</a>
+        <span onClick={navigateToLogo} style={{ cursor: "pointer" }}>
+          Bandage
+        </span>
       </h1>
     </div>
   );
--- src/components/HomeComponents/PracticeAdvice/PracticeSection.jsx
+++ src/components/HomeComponents/PracticeAdvice/PracticeSection.jsx
@@ -10,18 +10,39 @@ const PracticeSection = () => {
           Practice Advice
         </h3>
         <h2 className="my-4 p-4 md:p-0 mx-28 md:mx-0 text-3xl font-bold">
-          Featured Posts
+          Fashion Tips
         </h2>
-        <p className="mx-28 md:mx-0 text-xs text-[#737373] font-semibold">
+        <p className="mx-28 md:mx-0 text-sm text-[#737373] font-semibold">
           Problems trying to resolve the conflict between the two major realms
           of Classical physics: Newtonian mechanics
         </p>
       </div>
 
       <div className="md:flex md:justify-center md:items-center md:flex-wrap md:gap-1">
-        <PracticeCard />
-        <PracticeCard />
-        <PracticeCard />
+        <PracticeCard
+          image="https://stylecaster.com/wp-content/uploads/2016/04/street-style-camo.jpg?w=712&h=400&crop=1"
+          title="101 Fashion Tips & Tricks"
+          description="Discover the essential fashion tips and tricks every girl should know."
+          date="15 July 2024"
+          comments="5"
+          url="https://stylecaster.com/fashion/fashion-tips/164491/101-fashion-tips-tricks-every-girl-should-know/"
+        />
+        <PracticeCard
+          image="https://www.boredpanda.com/blog/wp-content/uploads/2023/05/style-tips_3-645a14f3221f2__700.jpg"
+          title="47 Style Tips To Look Your Best"
+          description="Explore a wide range of style tips for a modern look."
+          date="18 July 2024"
+          comments="8"
+          url="https://www.boredpanda.com/style-tips/"
+        />
+        <PracticeCard
+          image="https://cdn.suitdirect.co.uk/upload/blog/bcc06-man_overdressed_with_suit.jpg"
+          title="Top 10 Casual Fashion Tips"
+          description="Check out the top casual fashion tips for men."
+          date="22 July 2024"
+          comments="12"
+          url="https://www.suitdirect.co.uk/styleguide/features/top-10-casual-fashion-tips-for-men"
+        />
       </div>
     </div>
   );
